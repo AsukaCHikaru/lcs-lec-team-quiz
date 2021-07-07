47e8ba9ae2eb3bc4925877992a18579d4f0301ff
Asuka Wang <hungmao.wang@linecorp.com>
2021-07-07 16:58:26 +0900

    fix: build script

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 export default {
   input: 'src/main.js',
   output: {
-    file: 'dist/bundle.js',
+    file: 'public/bundle.js',
     format: 'iife',
     name: 'app',
   },
