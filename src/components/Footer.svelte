5569dddc9d5ed160a34fb2a0ab3d679f1b552361
Asuka Wang <hungmao.wang@linecorp.com>
2021-07-08 18:53:23 +0900

    add: footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Fa from 'svelte-fa';
+  import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+</script>
+
+<div class="footer-wrapper">
+  <a href="https://twitter.com/asukachikaru" rel="noreferrer noopener" target="_blank"><Fa icon={faTwitter} /></a>
+  <a href="https://github.com/AsukaCHikaru/lcs-lec-flag-quiz" rel="noreferrer noopener" target="_blank"><Fa icon={faGithub} /></a>
+</div>
+
+<style>
+  .footer-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  a {
+    margin: 0 10px;
+    font-size: 20px;
+  }
+</style>
--- src/components/Layout.svelte
+++ src/components/Layout.svelte
@@ -2,21 +2,31 @@
   import Title from './Title.svelte';
   import Quiz from './Quiz.svelte';
   import Rules from './Rules.svelte';
+  import Footer from './Footer.svelte';
 </script>
 
 <div class="layout-container">
-  <Title />
-  <Quiz />
-  <Rules />
+  <div class="content-container">
+    <Title />
+    <Quiz />
+    <Rules />
+  </div>
+  <Footer />
 </div>
 
 <style>
   .layout-container {
     height: calc(100% - 4em);
-    width: calc(100% - 4em);
+    max-width: 800px;
+    margin: auto;
+    padding: 2em 0;
+    display: flex;
+    flex-direction: column;
+  }
+  .content-container {
+    flex-grow: 1;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    padding: 2em;
   }
 </style>
