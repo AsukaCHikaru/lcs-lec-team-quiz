72d2dac71329123464c247cab05e49b7de3ef37f
Asuka Wang <hungmao.wang@linecorp.com>
2021-07-28 12:21:36 +0900

    fix: rules wording

--- src/components/Rules.svelte
+++ src/components/Rules.svelte
@@ -1,10 +1,10 @@
 <div class="rules-container">
   <h3 class="rules-title">Rules</h3>
   <ul class="rules-ul">
-    <li>Including NALCS, LCS, EULCS, LEC teams. Academy rosters are not included.</li>
+    <li>NALCS, LCS, EULCS, LEC teams, excluding academy.</li>
     <li>From 2013 spring to 2021 summer.</li>
-    <li>If a roster exists for over a split, the answer is the first split in that period.</li>
     <li>Data is based on <a href="https://lol.fandom.com/wiki/League_of_Legends_Esports_Wiki" rel="noreferrer noopener" target="_blank">Leaguepedia</a>.</li>
+    <li>Exclusive answer for every quiz. If there are invalid quizzes (eg. multiple answers), I'm sorry :p</li>
   </ul>
 </div>
 
